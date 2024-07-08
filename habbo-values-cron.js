c47620ba56196327b9ffac6389dfef198e9e3ecd
jordmax12 <jordmax12@gmail.com>
2024-07-08 00:14:32 -0500

    turn off cron

--- habbo-values-cron.js
+++ habbo-values-cron.js
@@ -135,6 +135,9 @@ const config = {
   headers: {
     'User-Agent':
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    Referer: 'https://originvalues.com/',
+    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
   },
 };
 
